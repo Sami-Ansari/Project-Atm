f1a876222724f0ea45bc588224dea819c6ddeefc
Sami-Ansari <118760264+Sami-Ansari@users.noreply.github.com>
2023-01-02 19:33:39 +0500

    Add files via upload

---
+++ Atm.js
@@ -0,0 +1,80 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+import showBanner from "node-banner";
+(async () => {
+    await showBanner('ATM', 'Habib Bank ATM ', "red");
+})();
+setTimeout(() => {
+    (async () => {
+        const usersInput = await inquirer.prompt([
+            {
+                name: "userId",
+                message: "Enter your user Id",
+                type: "input"
+            },
+            {
+                name: "userPin",
+                message: "Enter your Pin number",
+                type: "password"
+            },
+        ]);
+        const userData = {
+            userId: usersInput.userId,
+            userPin: usersInput.userPin,
+            amount: Math.floor(Math.random() * 100000 + 1)
+        };
+        const selectedOpt = await inquirer.prompt([
+            {
+                type: "list",
+                name: "accountType",
+                choices: ["Current", "Saving"],
+                message: "Select Your Account Type: ",
+            },
+            {
+                name: "Options",
+                type: "list",
+                choices: ["Fast Cash", "Amount"],
+                message: "Select your transaction ",
+            },
+        ]);
+        if (selectedOpt.Options === "Amount") {
+            console.log("Your current Amount: ", userData.amount);
+            const enteramount = await inquirer.prompt([
+                {
+                    message: "Enter the amount",
+                    name: "amount",
+                    type: "number",
+                    validate: (input) => {
+                        if (input > userData.amount) {
+                            return chalk.red("Insufficent Balance");
+                        }
+                        else {
+                            return true;
+                        }
+                    }
+                }
+            ]);
+            userData.amount -= enteramount.amount;
+            console.log(chalk.blackBright("Remaining Balance ", userData.amount));
+        }
+        else if (selectedOpt.Options == "Fast Cash") {
+            console.log("Your current Amount: ", userData.amount);
+            const Selectamount = await inquirer.prompt([
+                {
+                    type: "list",
+                    name: "amount1",
+                    choices: [500, 1000, 5000, 10000, 20000],
+                    message: "Select your Amount: "
+                }
+            ]);
+            if (userData.amount > Selectamount.amount1) {
+                userData.amount -= Selectamount.amount1;
+                console.log(chalk.blueBright("Remaining Balance ", userData.amount));
+            }
+            else {
+                console.log(chalk.red("Insufficent Balance"));
+            }
+        }
+    })();
+}, 1000);
